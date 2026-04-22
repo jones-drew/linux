84a25e05b8cbecdaa29608700e3c7162cd663413
Andrew Jones <andrew.jones@oss.qualcomm.com>
2026-04-22 16:45:04 -0500

    iommu/riscv: Report IOMMU_CAP_VIRT_MSI_ISOLATION
    Report IOMMU_CAP_VIRT_MSI_ISOLATION when supported.
    
    Signed-off-by: Andrew Jones <andrew.jones@oss.qualcomm.com>

--- drivers/iommu/riscv/iommu.c
+++ drivers/iommu/riscv/iommu.c
@@ -1484,9 +1484,14 @@ static struct iommu_group *riscv_iommu_device_group(struct device *dev)
 
 static bool riscv_iommu_capable(struct device *dev, enum iommu_cap cap)
 {
+	struct riscv_iommu_device *iommu = dev_to_iommu(dev);
+
 	switch (cap) {
 	case IOMMU_CAP_CACHE_COHERENCY:
 		return true;
+	case IOMMU_CAP_VIRT_MSI_ISOLATION:
+		return imsic_enabled() &&
+		       !!(iommu->caps & RISCV_IOMMU_CAPABILITIES_MSI_FLAT);
 	default:
 		return false;
 	}
