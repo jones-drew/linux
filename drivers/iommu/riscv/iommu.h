1d85e075f8c6a2cf8f92af19ea783b7f26fe4911
Andrew Jones <andrew.jones@oss.qualcomm.com>
2026-04-28 13:17:05 -0500

    iommu/riscv: Use g-stage for DMA to enable msi remapping
    The MSI table cannot be used unless DC.iohgatp is non-BARE (spec
    section 2.1.3). So just use g-stage instead of s-stage for DMA
    in order to enable MSI remapping.
    
    Signed-off-by: Andrew Jones <andrew.jones@oss.qualcomm.com>

--- drivers/iommu/riscv/iommu.h
+++ drivers/iommu/riscv/iommu.h
@@ -33,6 +33,7 @@ struct riscv_iommu_domain {
 	struct list_head bonds;
 	spinlock_t lock;		/* protect bonds list updates. */
 	int pscid;
+	int gscid;
 	struct riscv_iommu_msipte *msi_root;
 	u64 msi_addr_mask;
 	u64 msi_addr_pattern;
